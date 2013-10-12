55fc5526270078c611468a5f9f11f547e0024f10
root <root@sundberg-beagle>
2013-10-12 21:59:34 +0300

    Add open ssh pipe to crontab

---
+++ root/open_ssh_pipe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+target_host=$1
+target_port=$2
+
+if [[ -z "$target_host" || -z "$target_port" ]] ; then
+   echo "USAGE: $0 <target host> <target port>"
+   exit 1
+fi
+
+while [ true ] ; do
+   ssh -g -R $target_port:localhost:22 beagle@${target_host} -N
+   sleep 5
+done
+
+
