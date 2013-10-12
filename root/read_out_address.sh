9f963eadd1fa998ef858f51a3ab240792ee891d7
Pauli Sundberg <me@beagleboard>
2013-10-12 23:12:04 +0300

    Pretty speak, please

--- root/read_out_address.sh
+++ root/read_out_address.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-ifconfig eth0|grep "inet addr"|cut -f 2 -d :|cut -f 1 -d ' '|espeak -s 100
+OUTPUT=$(ifconfig eth0|grep "inet addr"|cut -f 2 -d :|cut -f 1 -d ' ')
+
+if [ -z "$OUTPUT" ]; then
+   OUTPUT="Cannot get proper address"
+else
+   OUTPUT="My address is  $OUTPUT"
+fi
+
+echo $OUTPUT |espeak -s 100
 
