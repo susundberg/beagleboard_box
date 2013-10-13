2a44754dea1c1bca9a074bd4ccd263ccce14bf5e
Pauli Sundberg <me@beagleboard>
2013-10-13 16:24:40 +0300

    New iptables rules

--- root/iptables_rules.sh
+++ root/iptables_rules.sh
@@ -1,3 +1,4 @@
+
 # 1. Delete all existing rules
 iptables -F
 
@@ -5,8 +6,19 @@ iptables -F
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
+iptables -X logdrop
 
 # 3. Allow incoming SSH
+iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+
+# 4. Add logging
+iptables -N logdrop
+iptables -A logdrop -m limit --limit 5/m --limit-burst 10 -j LOG
+iptables -A logdrop -j DROP
+
+iptables -A INPUT -j logdrop
+iptables -A FORWARD -j logdrop
 
